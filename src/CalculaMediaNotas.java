828293b6dbf38e146c2d8789dd6a429eb62c719b
Amanda Zaine <amandazaine@outlook.com>
2023-06-19 23:35:43 -0300

    modulo 10

--- src/CalculaMediaNotas.java
+++ src/CalculaMediaNotas.java
@@ -1,6 +1,16 @@
 public class CalculaMediaNotas {
 
-    public double calcularMediaNotas(double nota1, double nota2, double nota3, double nota4){
-        return (nota1 + nota2 + nota3 + nota4)/4;
+    public String calcularMediaNotas(int nota1, int nota2, int nota3, int nota4){
+        int media = (nota1 + nota2 + nota3 + nota4)/4;
+        String statusAluno;
+
+        if (media >= 7) {
+            statusAluno = "Aprovado";
+        } else if (media >= 5) {
+            statusAluno = "Em recuperacao";
+        } else {
+            statusAluno = "Reprovado";
+        }
+        return statusAluno;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         CalculaMediaNotas calculaMediaNotas = new CalculaMediaNotas();
-        double mediaNotas = calculaMediaNotas.calcularMediaNotas(5.0, 4.0, 8.0, 7.0);
-        System.out.printf("A media de notas é " + mediaNotas);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        int nota1 = scanner.nextInt();
+
+        System.out.println("Digite a segunda nota: ");
+        int nota2 = scanner.nextInt();
+
+        System.out.println("Digite a terceira nota: ");
+        int nota3 = scanner.nextInt();
+
+        System.out.println("Digite a quarta nota: ");
+        int nota4 = scanner.nextInt();
+
+        String mediaNotas = calculaMediaNotas.calcularMediaNotas(nota1, nota2, nota3, nota4);
+        System.out.printf("O Aluno está " + mediaNotas);
     }
 }
