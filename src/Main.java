b79ef3dcc17d6d6d30f05bbdcb876f6658e8a977
Amanda Zaine <amandazaine@outlook.com>
2023-06-21 01:54:45 -0300

    modulo 9

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        CalculaMediaNotas calculaMediaNotas = new CalculaMediaNotas();
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a primeira nota: ");
-        int nota1 = scanner.nextInt();
+        System.out.println("Digite um numero: ");
+        int num1 = scanner.nextInt();
 
-        System.out.println("Digite a segunda nota: ");
-        int nota2 = scanner.nextInt();
+        Integer num2 = num1;
 
-        System.out.println("Digite a terceira nota: ");
-        int nota3 = scanner.nextInt();
-
-        System.out.println("Digite a quarta nota: ");
-        int nota4 = scanner.nextInt();
-
-        String mediaNotas = calculaMediaNotas.calcularMediaNotas(nota1, nota2, nota3, nota4);
-        System.out.printf("O Aluno está " + mediaNotas);
+        System.out.printf("O numero é " + num2);
     }
 }
